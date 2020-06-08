64582a2a79200dc555d06b3c2701d489664d1072
João Dionisio <jbdionisio@hotmail.com.br>
2020-06-07 21:34:01 -0300

    add game features and check win condition

--- App.js
+++ App.js
@@ -4,18 +4,27 @@ import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
+import GameoverScreen from './screens/GameoverScreen';
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameoverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
   };
 
   let content = <StartGameScreen onStartGame={startGameHandler} />;
 
-  if(userNumber)
-    content = <GameScreen userChoice={userNumber} />;
+  if (userNumber && guessRounds <= 0)
+    content = <GameScreen userChoice={userNumber} onGameOver={gameoverHandler} />;
+  else if (guessRounds > 0)
+    content = <GameoverScreen />
 
   return (
     <View style={styles.screen}>
